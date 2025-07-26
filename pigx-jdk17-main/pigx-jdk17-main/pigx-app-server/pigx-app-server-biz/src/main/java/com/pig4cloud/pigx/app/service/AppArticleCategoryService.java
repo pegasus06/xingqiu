3dee76ccd07e1498d1628d0958a5181e22bacde7
zhourui <1010656935@qq.com>
2025-07-26 17:35:23 +0800

    Delete pigx-jdk17-main directory

--- pigx-jdk17-main/pigx-jdk17-main/pigx-app-server/pigx-app-server-biz/src/main/java/com/pig4cloud/pigx/app/service/AppArticleCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.pig4cloud.pigx.app.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.pig4cloud.pigx.app.api.entity.AppArticleCategoryEntity;
-
-public interface AppArticleCategoryService extends IService<AppArticleCategoryEntity> {
-
-}
--- pigx-jdk17-main/pigx-jdk17-main/pigx-app-server/pigx-app-server-biz/src/main/java/com/pig4cloud/pigx/app/service/AppPageService.java
+++
@@ -1,8 +0,0 @@
-package com.pig4cloud.pigx.app.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.pig4cloud.pigx.app.api.entity.AppPageEntity;
-
-public interface AppPageService extends IService<AppPageEntity> {
-
-}
